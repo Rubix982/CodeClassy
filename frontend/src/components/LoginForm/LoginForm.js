4ca2a05ddd5f320ee060d737a1d61c6c9fed4989
TashikMoin <tashikmoinsheikh@gmail.com>
2021-11-09 00:42:31 +0500

    updated login form from stratch

--- frontend/src/components/LoginForm/LoginForm.js
+++ frontend/src/components/LoginForm/LoginForm.js
@@ -1,47 +1,61 @@
 import React from 'react'
 import LoginFormStyling from '../../../styles/LoginForm/LoginForm.module.css'
-import { Grid,Paper, Link } from '@material-ui/core'
-import TextField from '@mui/material/TextField';
+import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button';
+import Link from 'next/link'
+import Box from '@mui/material/Box';
 
 
 const LoginForm= () =>
 {
-    const paperStyle={padding :20,height:'80vh',width:500, margin:"10vh auto"}
     return(
-        <Grid>
-            <Paper elevation={15} style={paperStyle}>
-                <Grid align='center'>
-                    <h1 className={LoginFormStyling.mainheading}> CodeClassy </h1>
-                    <h3 className={LoginFormStyling.subheading}>Let's get started.</h3>
-                </Grid>
+        <div className={LoginFormStyling.container}>
+            <div className={LoginFormStyling.form}>
 
-                <TextField style={{ marginTop: '5%', borderColor: 'white' }} required id="outlined-required" label="Email" placeholder='Email'
-                fullWidth/>
+                <div className={LoginFormStyling.formheader}>
+                    <h1 className={LoginFormStyling.logo}>
+                        Codeclassy
+                    </h1>
 
-                <TextField className={LoginFormStyling.textbox} required id="outlined-password-input" label="Password" placeholder='Password' 
-                type='password' fullWidth
-                />
-
-                <div className={LoginFormStyling.forgetpassword}>
-                    <Link style={{ textDecoration: 'none'}} href="#" >
-                        Forgot password?
-                    </Link>
-                </div>
-
-                <div className={LoginFormStyling.loginbuttoncontainer}>
-                    <Button className={LoginFormStyling.loginbutton} type='submit' color='primary' variant="contained"  fullWidth>
+                    <h2>
                         Sign in
-                    </Button>
+                    </h2>
+
+                    <h3>
+                        Use your Codeclassy Account
+                    </h3>
                 </div>
 
-                <div className={LoginFormStyling.createaccount}>
-                    <Link style={{ textDecoration: 'none'}} href="#" >
-                        Create account 
+                <div className={LoginFormStyling.inputs}>
+
+                    <div className={LoginFormStyling.textinput}>
+                        <TextField required id="outlined-required" label="Email" fullWidth />
+                    </div>
+
+                    <div className={LoginFormStyling.textinput}>
+                        <TextField required id="outlined-required" label="Password" type="password" fullWidth />
+                    </div>
+
+                    <Link href="/forgotpassword">
+                        <a className={LoginFormStyling.forgetpassword}> Forgot password? </a>
                     </Link>
+
                 </div>
-            </Paper>
-        </Grid>
+
+                <div className={LoginFormStyling.formfooter}>
+                    
+                    <Link href="/forgotpassword">
+                        <a className={LoginFormStyling.createaccount}> Create account </a>
+                    </Link>
+
+                    <Box mt={7} ml={27}>
+                        <Button style={{textTransform: 'capitalize'}} size="medium" variant="contained">Sign in</Button>
+                    </Box>
+                    
+                </div>
+
+            </div>
+        </div>
     )
 }
 
---
+++ frontend/src/components/RegisterForm/RegisterForm.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import RegisterFormStyling from '../../../styles/RegisterForm/RegisterForm.module.css'
+
+
+const RegisterForm= () =>
+{
+    return(
+        <div className={RegisterFormStyling.container}>
+            <div className={RegisterFormStyling.form}>
+                
+            </div>
+        </div>
+    )
+}
+
+export default RegisterForm
