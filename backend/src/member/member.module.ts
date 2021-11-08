832fef5f0203a597ceb0b1be6ad4c56518464f8d
Muhammad Hassan Zahid <hassanzhd2000@gmail.com>
2021-11-09 01:43:06 +0500

    updated MemberService for AuthService implementations

--- backend/src/member/member.module.ts
+++ backend/src/member/member.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Member } from 'src/entities/member.entity';
+import { MemberVerificationModule } from 'src/member-verification/member-verification.module';
 import { MemberController } from './member.controller';
 import { MemberService } from './member.service';
 @Module({
-  imports: [TypeOrmModule.forFeature([Member])],
+  imports: [TypeOrmModule.forFeature([Member]), MemberVerificationModule],
   controllers: [MemberController],
   providers: [MemberService],
-  exports: [],
+  exports: [MemberService],
 })
 export class MemberModule {}
--- backend/src/member/member.service.ts
+++ backend/src/member/member.service.ts
@@ -1,6 +1,8 @@
-import { Get, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { MemberVerification } from 'src/entities/member-verification.entity';
 import { Member } from 'src/entities/member.entity';
+import { MemberVerificationService } from 'src/member-verification/member-verification.service';
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -8,9 +10,30 @@ export class MemberService {
   constructor(
     @InjectRepository(Member)
     private readonly memberRepository: Repository<Member>,
+    private readonly memberVerificationService: MemberVerificationService,
   ) {}
+
   async findAll() {
     const members = await this.memberRepository.find();
     return members;
+  }
+
+  async createMember(__member: Member) {
+    const memberWithEmail = await this.memberRepository.findOne(__member.email);
+    if (!memberWithEmail) {
+      const verificationEntity: MemberVerification =
+        await this.memberVerificationService.createVerificationEntity();
+      __member.verification = verificationEntity;
+      await this.memberRepository.save(__member);
+    } else {
+      throw new BadRequestException(['Account present for specified email']);
+    }
+  }
+
+  async findOne(__email: string) {
+    const member = this.memberRepository.findOne(__email, {
+      relations: ['verification'],
+    });
+    return member;
   }
 }
