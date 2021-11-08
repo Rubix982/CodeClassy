718807d1dbba50ea60857c609aa7b129373ee7c9
Muhammad Hassan Zahid <hassanzhd2000@gmail.com>
2021-11-09 01:38:45 +0500

    added verifyHashedPassword in CryptoService

--- backend/src/crypto/crypto.service.ts
+++ backend/src/crypto/crypto.service.ts
@@ -9,9 +9,18 @@ export class CryptoService {
     const hashString = randomBytes(__length).toString('hex');
     return hashString;
   }
+
   async generateHashedPassword(__password: string) {
     const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(__password, salt);
     return hashedPassword;
+  }
+
+  async verifyHashedPassword(__password: string, __hashedPassword) {
+    const isPasswordVerified: boolean = await bcrypt.compare(
+      __password,
+      __hashedPassword,
+    );
+    return isPasswordVerified;
   }
 }
