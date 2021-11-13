5846468d78acb37e7402ab346de269679e21b64f
Saif Ul Islam <saifulislam84210@gmail.com>
2021-11-13 21:08:44 +0500

    Init code for the classroom
    Signed-off-by: Saif Ul Islam <saifulislam84210@gmail.com>

--- frontend/pages/classroom/[class_id].js
+++ frontend/pages/classroom/[class_id].js
@@ -1,8 +1,79 @@
 import React from 'react';
+
+import styles from '../../styles/Classroom/[class_id].module.css';
+
 import Navbar from '../../src/components/Navbar/Navbar';
+import CardMedia from '../../src/components/Classroom/CardMedia';
+
+import {
+    Grid
+} from '@mui/material';
 
 export default function Classroom() {
     return (
-        <Navbar />
+        <div>
+            <Navbar />
+            <Grid
+                container
+                direction="column"
+                justifyContent="center"
+                alignItems="stretch"
+            >
+                <Grid item xs={6}>
+                    <div className={styles.headerContainer}>
+                        <div className={styles.titleContainer}>
+                            <span className={styles.classroomName}>Design, Defects & Restructuring</span>
+                            <span className={styles.teacherName}>Sayed Yousuf</span>
+                            <span className={styles.classroomDescription}>The presence of design defects in object oriented software can have a severe impact on the quality of software.The detection and correction of design ...</span>
+                        </div>
+                    </div>
+                </Grid>
+
+                <Grid item xs={2}>
+                    <Grid
+                        container
+                        direction="row"
+                        justifyContent="space-between"
+                        alignItems="stretch"
+                        className={styles.gridContainerStyling}
+                    >
+                        <Grid item>
+                            <span className={styles.sectionStyling}>Sections</span>
+                        </Grid>
+
+                        <Grid item>
+                            <div className={styles.addSectionButtonStyling}>
+                                <span className={styles.addSectionStyling}>+ Add</span>
+                            </div>
+                        </Grid>
+                    </Grid>
+                </Grid>
+
+                <Grid item xs={4}>
+                    <Grid
+                        container
+                        direction="row"
+                        justifyContent="space-between"
+                        alignItems="stretch"
+                        className={styles.gridContainerStyling}
+                        spacing={10}
+                    >
+
+                        <Grid item xs={4}>
+                            <CardMedia />
+                        </Grid>
+
+                        <Grid item xs={4}>
+                            <CardMedia />
+                        </Grid>
+
+                        <Grid item xs={4}>
+                            <CardMedia />
+                        </Grid>
+
+                    </Grid>
+                </Grid>
+            </Grid>
+        </div >
     )
 }
---
+++ frontend/src/components/Classroom/CardMedia.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+
+import styles from "../../../styles/Classroom/CardMedia.module.css";
+
+import {
+    Card,
+    CardContent
+} from '@mui/material';
+
+export default function CardMedia() {
+    return (
+        <Card sx={{ minWidth: 275 }}>
+            <CardContent className={styles.cardContentStyling}>
+                <div className={`${styles.profileLogoStyling}`}>
+                    <span className={styles.profileTextStyling}>SUI</span>
+                </div>
+                <p className={styles.sectionNameStyling}>
+                    7A
+                </p>
+            </CardContent>
+            <div className={styles.designationStyling}>
+                <p>
+                    Assigned to "<a href="mailto:saifulislam84210@gmail.com"> saifulislam84210@gmail.com</a>"
+                </p>
+            </div>
+        </Card>
+    );
+}
