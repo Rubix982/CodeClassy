62f4f878a8f20e584f64d7e8e4b4eaf327268b91
Saif Ul Islam <saifulislam84210@gmail.com>
2021-11-19 12:33:39 +0500

    Fixed index styles, removed membership view from home, add default code from bloggr
    Signed-off-by: Saif Ul Islam <saifulislam84210@gmail.com>

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,13 +1,61 @@
-import Head from 'next/head'
-import MembersView from '../src/components/MembersView/MembersView'
+import React from "react";
 
-export default function Home() {
+import styles from "../styles/index.module.css";
+
+import { Grid, Button } from '@mui/material';
+
+export default function Index() {
   return (
-    <div>
-      <MembersView/>  
-      {/* adding this component here for testing purpose only.. it will be inside classroom people page 
-      along with the navbar  */}
-    </div>
-  )
-}
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      style={{ minHeight: '90vh', minWidth: '100vw', flexDirection: "row", justifyContent: 'center' }}
+    >
 
+      <Grid item xs={4}>
+        <div className={styles.mainCodeClassyTitle}>
+          <p>Welcome To CodeClassy!</p>
+
+        </div>
+
+        <div className={styles.mainCodeClassyContent}>
+          <p>The world's biggest learning platform! here.</p>
+
+          <p>Get started today!</p>
+        </div>
+        <Grid
+          container
+          direction="row"
+          justifyContent="space-between"
+          alignItems="flex-end"
+        >
+
+          <Grid item xs={4} alignItems="center" justifyContent="center" className={styles.actionButton}>
+            <Grid item alignItems="center" justifyContent="center">
+              <Button href="/login">
+                <span className={styles.actionButtonContent}>Login</span>
+              </Button>
+            </Grid>
+          </Grid>
+
+          <Grid item xs={4}>
+
+          </Grid>
+
+          <Grid item xs={4} className={styles.actionButton}>
+            <Grid alignItems="center" justifyContent="center" >
+              <Button href="/register">
+                <span className={styles.actionButtonContent}>Register</span>
+              </Button>
+            </Grid>
+          </Grid>
+
+        </Grid>
+      </Grid>
+
+    </Grid>
+  );
+}
