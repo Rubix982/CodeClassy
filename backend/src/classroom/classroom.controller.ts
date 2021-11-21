4c86e278af8e5979a03618bf051f9cf55b0b7bb0
Muhammad Hassan Zahid <hassanzhd2000@gmail.com>
2021-11-21 05:06:55 +0500

    implemented classroom creation unit

--- backend/src/classroom/classroom.controller.ts
+++ backend/src/classroom/classroom.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { JWTPayload } from 'src/auth/signin.dto';
+import { RequestDecodedMember } from 'src/decorators/member.decorator';
+import { ClassroomGuard } from './classroom.guard';
 import { ClassroomService } from './classroom.service';
+import { CreateClassroomDTO } from './create.dto';
 
+@UseGuards(ClassroomGuard)
 @Controller('classroom')
 export class ClassroomController {
   constructor(private readonly classroomService: ClassroomService) {}
+
+  @Post()
+  async create(
+    @Body() __requestBody: CreateClassroomDTO,
+    @RequestDecodedMember() __teacher: JWTPayload,
+  ) {
+    await this.classroomService.createClassroom(__teacher.email, __requestBody);
+    return { msg: `Successfully created classroom: ${__requestBody.name}` };
+  }
 }
--- backend/src/classroom/classroom.module.ts
+++ backend/src/classroom/classroom.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 import { Classroom } from 'src/entities/classroom.entity';
+import { TeacherModule } from 'src/teacher/teacher.module';
 import { ClassroomController } from './classroom.controller';
 import { ClassroomService } from './classroom.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Classroom])],
+  imports: [TypeOrmModule.forFeature([Classroom]), AuthModule, TeacherModule],
   controllers: [ClassroomController],
   providers: [ClassroomService],
   exports: [],
