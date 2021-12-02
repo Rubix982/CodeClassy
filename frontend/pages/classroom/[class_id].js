87d71d4501bff2735e3a11c69207f34a202e3f74
Saif Ul Islam <saifulislam84210@gmail.com>
2021-12-02 09:42:26 +0500

    Refactored class and section navigation, modal added, styling issues fixed, navbar is completely responsive, refactored modal for fade-in/fade-out
    Signed-off-by: Saif Ul Islam <saifulislam84210@gmail.com>

--- frontend/pages/classroom/[class_id].js
+++ frontend/pages/classroom/[class_id].js
@@ -3,10 +3,14 @@ import { useRouter } from 'next/router';
 
 import styles from '../../styles/Classroom/[class_id].module.css';
 
+import { styled } from '@mui/material/styles';
+
 import {
     Box,
     Tab,
-    Grid
+    AppBar,
+    Toolbar,
+    Avatar,
 } from "@mui/material";
 
 import {
@@ -15,8 +19,16 @@ import {
     TabPanel
 } from "@mui/lab";
 
+import {
+    deepOrange,
+} from "@mui/material/colors";
+
 import ClassroomInformation from "@components/Classroom/ClassroomInformation";
 import MembersView from '@components/MembersView/MembersView';
+
+const StyledToolbar = styled(Toolbar)(({ theme }) => ({
+    padding: 0,
+}));
 
 export default function Classroom() {
 
@@ -25,46 +37,35 @@ export default function Classroom() {
 
     const [value, setValue] = React.useState('1');
 
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
+    const handleChange = (event, newValue) => setValue(newValue);
 
     return (
-        <div>
-            <TabContext value={value}>
-                <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
-                    <TabList onChange={handleChange} aria-label="lab API tabs example" centered>
-                        <Tab label="Dashboard" value="1" />
-                        <Tab label="People" value="2" />
-                        <Tab>
-                            <Grid
-                                container
-                                direction="row"
-                                justifyContent="flex-end"
-                                alignItems="flex-start"
-                            >
-                                <ul className={styles.ulStyling}>
-                                    <li className={`${styles.iconStyling} ${styles.itemStyling}`}>
-                                        <div className={`${styles.profileLogoStyling}`}>
-                                            <span className={styles.profileTextStyling}>SUI</span>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </Grid>
-                        </Tab>
-                    </TabList>
-                </Box>
-                <TabPanel value="1" className={styles.tabPanelStyling}>
-                    <ClassroomInformation
-                        title="Design, Defects And Restructuring"
-                        teacher="Sayed Yousuf"
-                        description="The presence of design defects in object oriented software can have a severe impact on the quality of software.The detection and correction of design ..."
-                    />
-                </TabPanel>
-                <TabPanel value="2" className={styles.tabPanelStyling}>
-                    <MembersView />
-                </TabPanel>
-            </TabContext>
-        </div >
+        <TabContext value={value}>
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="static">
+                    <StyledToolbar className={styles.appBarStyling}>
+                        <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
+                            <TabList onChange={handleChange} aria-label="lab API tabs example" centered>
+                                <Tab label="Dashboard" value="1" className={styles.tabStyling} />
+                                <Tab label="People" value="2" className={styles.tabStyling} />
+                            </TabList>
+                        </Box>
+                        <Avatar sx={{ bgcolor: deepOrange[500] }} className={styles.profileLogoStyling}>
+                            S
+                        </Avatar>
+                    </StyledToolbar>
+                </AppBar>
+            </Box>
+            <TabPanel value="1" className={styles.tabPanelStyling}>
+                <ClassroomInformation
+                    title="Design, Defects And Restructuring"
+                    teacher="Sayed Yousuf"
+                    description="The presence of design defects in object oriented software can have a severe impact on the quality of software.The detection and correction of design ..."
+                />
+            </TabPanel>
+            <TabPanel value="2" className={styles.tabPanelStyling}>
+                <MembersView />
+            </TabPanel>
+        </TabContext>
     )
 }
--- frontend/pages/section/[section_id].js
+++ frontend/pages/section/[section_id].js
@@ -3,19 +3,33 @@ import { useRouter } from 'next/router';
 
 import styles from '../../styles/Section/[section_id].module.css';
 
+import { styled } from '@mui/material/styles';
+
 import {
     Box,
-    Tab
+    Tab,
+    AppBar,
+    Toolbar,
+    Avatar,
 } from "@mui/material";
 
 import {
     TabContext,
     TabList,
-    TabPanel 
+    TabPanel
 } from "@mui/lab";
+
+import {
+    deepOrange,
+} from "@mui/material/colors";
 
 import SectionInformation from '@components/Section/SectionInformation';
 import MembersView from '@components/MembersView/MembersView';
+import InvitationModal from '@components/Navbar/InvitationModal';
+
+const StyledToolbar = styled(Toolbar)(({ theme }) => ({
+    padding: 0,
+}));
 
 export default function Section() {
 
@@ -24,28 +38,34 @@ export default function Section() {
 
     const [value, setValue] = React.useState('1');
 
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
+    const handleChange = (event, newValue) => setValue(newValue);
 
     return (
-        <div>
-            <Box sx={{ width: '100%', typography: 'body1' }}>
-                <TabContext value={value}>
-                    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                        <TabList onChange={handleChange} aria-label="lab API tabs example" centered>
-                            <Tab label="Dashboard" value="1" />
-                            <Tab label="People" value="2" />
-                        </TabList>
-                    </Box>
-                    <TabPanel value="1" className={styles.tabPanelStyling}>
-                        <SectionInformation />
-                    </TabPanel>
-                    <TabPanel value="2" className={styles.tabPanelStyling}>
-                        <MembersView />
-                    </TabPanel>
-                </TabContext>
+        <TabContext value={value}>
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="static">
+                    <StyledToolbar className={styles.appBarStyling}>
+                        <Box sx={{ width: '100%', typography: 'body1' }}>
+                            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                                <TabList onChange={handleChange} aria-label="lab API tabs example" centered>
+                                    <Tab label="Stream" value="1" className={styles.tabStyling} />
+                                    <Tab label="People" value="2" className={styles.tabStyling} />
+                                </TabList>
+                            </Box>
+                        </Box>
+                        <InvitationModal />
+                        <Avatar sx={{ bgcolor: deepOrange[500] }} className={styles.profileLogoStyling}>
+                            S
+                        </Avatar>
+                    </StyledToolbar>
+                </AppBar>
             </Box>
-        </div>
+            <TabPanel value="1" className={styles.tabPanelStyling}>
+                <SectionInformation />
+            </TabPanel>
+            <TabPanel value="2" className={styles.tabPanelStyling}>
+                <MembersView />
+            </TabPanel>
+        </TabContext>
     )
 }
