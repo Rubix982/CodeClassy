8471368ee1467f7dbef20ff540efcd99b5d88a5b
Muhammad Hassan Zahid <hassanzhd2000@gmail.com>
2021-12-05 16:38:37 +0500

    added frontend/api wrapper for axios

---
+++ frontend/api/index.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+class API {
+  static instance;
+
+  constructor() {
+    this.options = { withCredentials: true };
+    this.baseUrl = "http://localhost:5000";
+  }
+
+  static getInstance() {
+    if (!this.instance) {
+      this.instance = new API();
+    }
+    return this.instance;
+  }
+
+  getFullRequestUrl(__requestUrl) {
+    return `${this.baseUrl}/${__requestUrl}`;
+  }
+
+  async get(__requestUrl) {
+    const fullRequestUrl = this.getFullRequestUrl(__requestUrl);
+    await axios.get(fullRequestUrl, this.options);
+  }
+
+  async post(__requestUrl, __data) {
+    const fullRequestUrl = this.getFullRequestUrl(__requestUrl);
+    await axios.post(fullRequestUrl, __data, this.options);
+  }
+}
+
+export default API;
