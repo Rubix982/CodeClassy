13a35cc5c5cb486ac40c7934386887f673174f06
Muhammad Hassan Zahid <hassanzhd2000@gmail.com>
2021-12-07 02:09:16 +0500

    added classroom.ID on response to use for routing on client end

--- backend/src/teacher/teacher.controller.ts
+++ backend/src/teacher/teacher.controller.ts
@@ -21,8 +21,14 @@ export class TeacherController {
     @RequestDecodedMember() __member: JWTPayload,
   ) {
     const teacher = await this.teacherService.findTeacher(__member.email);
-    await this.classroomService.createClassroom(teacher, __requestBody);
-    return { msg: `Successfully created classroom: ${__requestBody.name}` };
+    const classroomID = await this.classroomService.createClassroom(
+      teacher,
+      __requestBody,
+    );
+    return {
+      msg: `Successfully created classroom: ${__requestBody.name}`,
+      ID: classroomID,
+    };
   }
 
   @Get('feed')
