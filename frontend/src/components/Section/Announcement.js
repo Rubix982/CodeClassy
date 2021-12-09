525cdf1f8fba106847dc2a70d3ee4c759388c7fb
Tashik <k180142@nu.edu.pk>
2021-12-09 17:42:31 +0500

    removed comments

--- frontend/src/components/Section/Announcement.js
+++ frontend/src/components/Section/Announcement.js
@@ -12,27 +12,7 @@ import {
     Avatar
 } from "@mui/material";
 
-import {
-    red
-} from "@mui/material/colors";
 
-import Comment from './Comment';
-import PostComment from './PostComment';
-
-var comments = [
-    {
-        name: "S",
-        comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In viverra bibendum neque quis aliquam. Integer semper lorem vel tellus iaculis, at ultricies est euismod. Mauris ut gravida velit. Ut dapibus turpis ut sapien bibendum mattis. ed vehicula nulla eu venenatis mollis. Nullam pretium ante et turpis tempor efficitur. Aliquam leo purus, feugiat vitae pharetra convallis, ultrices vel dui. Nulla pharetra nisl vitae tellus cursus, pulvinar tempor purus tincidunt."
-    },
-    {
-        name: "T",
-        comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In viverra bibendum neque quis aliquam. Integer semper lorem vel tellus iaculis, at ultricies est euismod. Mauris ut gravida velit. Ut dapibus turpis ut sapien bibendum mattis. ed vehicula nulla eu venenatis mollis. Nullam pretium ante et turpis tempor efficitur. Aliquam leo purus, feugiat vitae pharetra convallis, ultrices vel dui. Nulla pharetra nisl vitae tellus cursus, pulvinar tempor purus tincidunt."
-    },
-    {
-        name: "M",
-        comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In viverra bibendum neque quis aliquam. Integer semper lorem vel tellus iaculis, at ultricies est euismod. Mauris ut gravida velit. Ut dapibus turpis ut sapien bibendum mattis. ed vehicula nulla eu venenatis mollis. Nullam pretium ante et turpis tempor efficitur. Aliquam leo purus, feugiat vitae pharetra convallis, ultrices vel dui. Nulla pharetra nisl vitae tellus cursus, pulvinar tempor purus tincidunt."
-    }
-]
 
 export default function Announcement() {
     return (
@@ -40,8 +20,8 @@ export default function Announcement() {
             <Grid
                 container
                 direction="column"
-                justifyContent="flex-start"
-                alignItems="flex-start"
+                justifyContent="center"
+                alignItems="center"
             >
                 <Card variant="outlined" className={styles.cardStyling}>
                     <CardHeader
@@ -60,16 +40,6 @@ export default function Announcement() {
                     </CardContent>
                 </Card>
 
-                <Grid style={{ textAlign: 'justify'}} item xs={12} sx={{ width: '100%' }}>
-                    {comments.map((comment, index) => {
-                        return (
-                            <div key={index}>
-                                <Comment comment={comment.comment} name={comment.name} />
-                            </div>
-                        );
-                    })}
-                    <PostComment name={'S'} />
-                </Grid>
             </Grid>
         </Container>
     )
