a45f898ac175cc6006ca17f47c9c59b8a5b76d37
Tashik <k180142@nu.edu.pk>
2021-12-20 02:28:33 +0500

    added grading and shuffling modes

--- frontend/src/components/AddNewQuestion/QuestionSettings.js
+++ frontend/src/components/AddNewQuestion/QuestionSettings.js
@@ -14,14 +14,13 @@ let categories =
     "Object-Oriented Programming"
 ]
 
-const QuestionSettings = ({questionsCategory, points, randomize}) =>
+const QuestionSettings = ({questionsCategory, points, randomize, shuffle, grading}) =>
 {
     const [category, setCategory] = React.useState(categories[0]);
-    const [selectedValue, setSelectedValue] = React.useState('Yes');
+    const [firstRadio, setFirstRadio] = React.useState('Yes');
+    const [secondRadio, setSecondRadio] = React.useState('Shuffle Matches');
+    const [thirdRadio, setThirdRadio] = React.useState('Off');
 
-    const handleChange = (event) => {
-      setSelectedValue(event.target.value);
-    };
 
 
     return(
@@ -77,8 +76,8 @@ const QuestionSettings = ({questionsCategory, points, randomize}) =>
                     <h4 style={{marginBottom: '10px'}}> Randomize Answers </h4>
                     <div>
                         <Radio
-                            checked={selectedValue === 'Yes'}
-                            onChange={handleChange}
+                            checked={firstRadio === 'Yes'}
+                            onChange={(e)=>setFirstRadio(e.target.value)}
                             value={'Yes'}
                             name="radio-buttons"
                             inputProps={{ 'aria-label': 'Yes' }}
@@ -88,13 +87,91 @@ const QuestionSettings = ({questionsCategory, points, randomize}) =>
 
                     <div>
                         <Radio
-                            checked={selectedValue === 'No'}
-                            onChange={handleChange}
+                            checked={firstRadio === 'No'}
+                            onChange={(e)=>setFirstRadio(e.target.value)}
                             value={'No'}
                             name="radio-buttons"
                             inputProps={{ 'aria-label': 'No' }}
                         />
                         <label> No </label>
+                    </div>
+                </div>)
+            }
+
+            { shuffle &&
+                (<div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'flex-start'}} 
+                className={settingsStyle.subContainer}>
+                    <h4 style={{marginBottom: '10px'}}> Shuffle Mode </h4>
+                    <div>
+                        <Radio
+                            checked={secondRadio === 'Shuffle matches only'}
+                            onChange={(e)=>setSecondRadio(e.target.value)}
+                            value={'Shuffle matches only'}
+                            name="radio-buttons"
+                            inputProps={{ 'aria-label': 'Shuffle matches only' }}
+                        />
+                        <label> Shuffle matches only </label>
+                    </div>
+
+                    <div>
+                        <Radio
+                            checked={secondRadio === 'Shuffle clues only'}
+                            onChange={(e)=>setSecondRadio(e.target.value)}
+                            value={'Shuffle clues only'}
+                            name="radio-buttons"
+                            inputProps={{ 'aria-label': 'Shuffle clues only' }}
+                        />
+                        <label> Shuffle clues only</label>
+                    </div>
+
+                    <div>
+                        <Radio
+                            checked={secondRadio === 'Shuffle clues and matches'}
+                            onChange={(e)=>setSecondRadio(e.target.value)}
+                            value={'Shuffle clues and matches'}
+                            name="radio-buttons"
+                            inputProps={{ 'aria-label': 'Shuffle clues and matches' }}
+                        />
+                        <label> Shuffle clues and matches </label>
+                    </div>
+                </div>)
+            }
+
+            { grading &&
+                (<div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'flex-start'}} 
+                className={settingsStyle.subContainer}>
+                    <h4 style={{marginBottom: '10px'}}> Grading Scale </h4>
+                    <div>
+                        <Radio
+                            checked={thirdRadio === 'Off'}
+                            onChange={(e)=>setThirdRadio(e.target.value)}
+                            value={'Off'}
+                            name="radio-buttons"
+                            inputProps={{ 'aria-label': 'Off' }}
+                        />
+                        <label> Off </label>
+                    </div>
+
+                    <div>
+                        <Radio
+                            checked={thirdRadio === 'Partial with deduction'}
+                            onChange={(e)=>setThirdRadio(e.target.value)}
+                            value={'Partial with deduction'}
+                            name="radio-buttons"
+                            inputProps={{ 'aria-label': 'Partial with deduction' }}
+                        />
+                        <label> Partial with deduction </label>
+                    </div>
+
+                    <div>
+                        <Radio
+                            checked={thirdRadio === 'Partial without deduction'}
+                            onChange={(e)=>setThirdRadio(e.target.value)}
+                            value={'Partial without deduction'}
+                            name="radio-buttons"
+                            inputProps={{ 'aria-label': 'Partial without deduction' }}
+                        />
+                        <label> Partial without deduction </label>
                     </div>
                 </div>)
             }
