e101478d8c61bae3d9f7dadae3c1be5a24d53ccb
Saif Ul Islam <saifulislam84210@gmail.com>
2022-01-07 10:05:36 +0500

    Wrapped question bank in ensure-authenticated
    Signed-off-by: Saif Ul Islam <saifulislam84210@gmail.com>

--- frontend/pages/questionbank/newquestion.js
+++ frontend/pages/questionbank/newquestion.js
@@ -1,13 +1,25 @@
-import React from 'react'
-import AddNewQuestion from '../../src/components/AddNewQuestion/AddNewQuestion'
+// React imports
+import React from "react";
 
-const NewQuestion= () =>
-{
-    return(
-        <div>
-            <AddNewQuestion/>
-        </div>
-    )
-}
+// NextJS imports
+import Head from "next/head";
+
+// Component imports
+import AddNewQuestion from "@components/AddNewQuestion/AddNewQuestion";
+import EnsureAuthenticated from "@components/Auth/ensure-authenticated";
+
+const NewQuestion = () => {
+  return (
+    <>
+      <Head>
+        <title>Question Bank</title>
+        <link rel="icon" href="/assets/images/vercel.svg" />
+      </Head>
+      <EnsureAuthenticated>
+        <AddNewQuestion />
+      </EnsureAuthenticated>
+    </>
+  );
+};
 
 export default NewQuestion;
