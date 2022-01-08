3f0c616edff8251c910332cf90c26639b48c1125
Saif Ul Islam <saifulislam84210@gmail.com>
2022-01-08 12:21:55 +0500

    Updated section page
    Signed-off-by: Saif Ul Islam <saifulislam84210@gmail.com>

--- frontend/pages/section/[id].js
+++ frontend/pages/section/[id].js
@@ -1,65 +1,170 @@
+// React imports
 import React from "react";
+
+// NextJS imports
 import { useRouter } from "next/router";
+import Link from "next/link";
+import Head from "next/head";
 
-import styles from "../../styles/Section/[section_id].module.css";
-
+// Styling imports
+import SectionPageStyling from "@styles/Section/[id].module.scss";
 import { styled } from "@mui/material/styles";
 
-import { Box, Tab, AppBar, Toolbar, Avatar } from "@mui/material";
-
+// MUI imports
+import {
+  Box,
+  Tab,
+  AppBar,
+  Toolbar,
+  Avatar,
+  Tooltip,
+  IconButton,
+  Menu,
+  MenuItem,
+  ListItemIcon,
+} from "@mui/material";
 import { TabContext, TabList, TabPanel } from "@mui/lab";
+import Settings from "@mui/icons-material/Settings";
+import Logout from "@mui/icons-material/Logout";
+import { red } from "@mui/material/colors";
 
-import { deepOrange } from "@mui/material/colors";
-
+// Component imports
 import SectionInformation from "@components/Section/SectionInformation";
 import MembersView from "@components/MembersView/MembersView";
-import InvitationModal from "@components/Tabbar/InvitationModal";
+import InvitationModal from "@components/Navbar/InvitationModal";
+
+// Redux imports
+import { connect } from "react-redux";
+
+// API imports
+import { getSectionFeed } from "redux/actions/section.action";
 
 const StyledToolbar = styled(Toolbar)(({ theme }) => ({
   padding: 0,
 }));
 
-export default function Section() {
+function SectionPage({ userFullName, userRole, isAuthenticated, isLoading }) {
   const router = useRouter();
   const { section_id } = router.query;
 
   const [value, setValue] = React.useState("1");
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
 
-  const handleChange = (event, newValue) => setValue(newValue);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleChange = (event, newValue) => {
+    event.preventDefault();
+    setValue(newValue);
+  };
 
   return (
-    <TabContext value={value}>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <StyledToolbar className={styles.appBarStyling}>
-            <Box sx={{ width: "100%", typography: "body1" }}>
-              <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-                <TabList
-                  onChange={handleChange}
-                  aria-label="lab API tabs example"
-                  centered
-                >
-                  <Tab label="Stream" value="1" className={styles.tabStyling} />
-                  <Tab label="People" value="2" className={styles.tabStyling} />
-                </TabList>
+    <>
+      <Head>
+        <title>Section</title>
+        <meta name="section page" content="Section page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <TabContext value={value}>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <StyledToolbar className={SectionPageStyling.appBarStyling}>
+              <Link href="/h">
+                <h1 className={SectionPageStyling.codeClassyStyling}>
+                  CodeClassy
+                </h1>
+              </Link>
+              <Box sx={{ width: "100%", typography: "body1" }}>
+                <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                  <TabList
+                    onChange={handleChange}
+                    aria-label="lab API tabs example"
+                    centered
+                  >
+                    <Tab
+                      label="Stream"
+                      value="1"
+                      className={SectionPageStyling.tabStyling}
+                    />
+                    <Tab
+                      label="People"
+                      value="2"
+                      className={SectionPageStyling.tabStyling}
+                    />
+                  </TabList>
+                </Box>
               </Box>
-            </Box>
-            <InvitationModal />
-            <Avatar
-              sx={{ width: 40, height: 40 }}
-              className={styles.profileLogoStyling}
-            >
-              TM
-            </Avatar>
-          </StyledToolbar>
-        </AppBar>
-      </Box>
-      <TabPanel value="1" className={styles.tabPanelStyling}>
-        <SectionInformation section={section_id} />
-      </TabPanel>
-      <TabPanel value="2" className={styles.tabPanelStyling}>
-        <MembersView />
-      </TabPanel>
-    </TabContext>
+
+              {/* Only display the "Plus" modal if the role
+              is that of a teacher */}
+              {userRole === "Teacher" && (
+                <Box>
+                  <InvitationModal />
+                </Box>
+              )}
+
+              <div>
+                <Box>
+                  <Tooltip title="Account settings">
+                    <IconButton
+                      onClick={handleClick}
+                      size="small"
+                      className={SectionPageStyling.icon}
+                    >
+                      <Avatar
+                        sx={{ bgcolor: red[500] }}
+                        className={SectionPageStyling.icon}
+                        {...stringAvatar(userFullName)}
+                      />
+                    </IconButton>
+                  </Tooltip>
+                </Box>
+                <Menu
+                  className={SectionPageStyling.avatarMenu}
+                  anchorEl={anchorEl}
+                  open={open}
+                  onClose={handleClose}
+                  onClick={handleClose}
+                >
+                  <MenuItem>
+                    <ListItemIcon>
+                      <Settings fontSize="small" />
+                    </ListItemIcon>
+                    Settings
+                  </MenuItem>
+                  <MenuItem>
+                    <ListItemIcon>
+                      <Logout fontSize="small" />
+                    </ListItemIcon>
+                    Sign out
+                  </MenuItem>
+                </Menu>
+              </div>
+            </StyledToolbar>
+          </AppBar>
+        </Box>
+        <TabPanel value="1" className={SectionPageStyling.tabPanelStyling}>
+          <SectionInformation section={section_id} />
+        </TabPanel>
+        <TabPanel value="2" className={SectionPageStyling.tabPanelStyling}>
+          <MembersView />
+        </TabPanel>
+      </TabContext>
+    </>
   );
 }
+
+const mapStateToProps = (state) => ({
+  userFullName: state.sectionReducer.userFullName,
+  userRole: state.sectionReducer.userRole,
+  isAuthenticated: state.sectionReducer.isAuthenticated,
+  isLoading: state.sectionReducer.isLoading,
+});
+
+export default connect(mapStateToProps, { getSectionFeed })(SectionPage);
