6f27861b8883267081cf858d97eb253057297e58
Saif ul Islam <saifulislam84210@gmail.com>
2022-01-11 00:49:15 -0500

    Removed unncessary/simplified imports, fixed typo quizes -> quizzes, formatted with prettier
    Signed-off-by: Saif ul Islam <saifulislam84210@gmail.com>

--- frontend/src/components/StudentHomePage/HomePage.js
+++ frontend/src/components/StudentHomePage/HomePage.js
@@ -1,38 +1,24 @@
-import React, {useState} from "react";
-import Box from "@mui/material/Box";
-import Avatar from "@mui/material/Avatar";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import IconButton from "@mui/material/IconButton";
-import Tooltip from "@mui/material/Tooltip";
-import Settings from "@mui/icons-material/Settings";
-import Logout from "@mui/icons-material/Logout";
-import { red } from "@mui/material/colors";
+import React, { useState } from "react";
 import HomePageStyling from "@styles/HomePage/HomePage.module.scss";
 import { getStudentFeed } from "redux/actions/student.action";
 import { connect } from "react-redux";
-import { TabPanel, a11yProps } from "../MaterialCustomComponents/TabPanel"
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import Button from "@mui/material/Button";
-import Feed from "./Feed"
-import Quizes from "./Quizes"
-import Assignments from "./Assignments"
-import RemoteCodeExecution from "./RemoteCodeExecution"
-import FeedSectionCard from "../FeedSectionCard/FeedSectionCard";
+import {
+  TabPanel,
+  a11yProps,
+} from "@components/MaterialCustomComponents/TabPanel";
+import { Tabs, Tab, Button } from "@mui/material";
+import Feed from "@components/StudentHomePage/Feed";
+import Quizzes from "@components/StudentHomePage/Quizzes";
+import Assignments from "@components/StudentHomePage/Assignments";
+import RemoteCodeExecution from "@components/StudentHomePage/RemoteCodeExecution";
 import Navbar from "@components/Navbar/Navbar";
 
-
-let tabsData = 
-[
-  {"name": "Feed", component: Feed}, 
-  {"name": "Quizes", component: Quizes },
-  {"name": "Assignments", component: Assignments },
-  {"name": "Remote Code Execution", component: RemoteCodeExecution}
-]
-
-
+let tabsData = [
+  { name: "Feed", component: Feed },
+  { name: "Quizzes", component: Quizzes },
+  { name: "Assignments", component: Assignments },
+  { name: "Remote Code Execution", component: RemoteCodeExecution },
+];
 
 const HomePage = ({ getStudentFeed, feedLoading, studentSections }) => {
   React.useEffect(() => {
@@ -47,67 +33,64 @@ const HomePage = ({ getStudentFeed, feedLoading, studentSections }) => {
   };
 
   return (
-    <div style={{display: 'flex', flexDirection: 'column'}}>
+    <div style={{ display: "flex", flexDirection: "column" }}>
       <Navbar />
 
       <div className={HomePageStyling.subTabMenu}>
-          <Tabs
-            TabIndicatorProps={{
-              style: {
-                display: "none",
-              },
-            }}
-            centered
-            value={value}
-            onChange={handleChange}
-            aria-label="basic tabs example"
-            style={{ height: "100%", display: 'flex', alignItems: 'center' }}
-          >
-            {tabsData.map((i, index) => {
-              let color = "grey";
-              if (currentTabID === i.name) {
-                color = "#000000";
-              }
-              return (
-                <Tab
-                  key={index}
-                  {...a11yProps(index)}
-                  component={() => (
-                    <Button
-                      style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        padding: "10px",
-                        margin: "18px",
-                        textAlign: "center",
-                        justifyContent: "center"
-                      }}
-                      onClick={() => {
-                        setValue(index);
-                        setCurrentTabID(i.name);
-                      }}
-                    >
-                      <label
-                        style={{ color: color, cursor: 'pointer' }}
-                      >
-                        {i.name}
-                      </label>
-                    </Button>
-                  )}
-                />
-              );
-            })}
-          </Tabs>
+        <Tabs
+          TabIndicatorProps={{
+            style: {
+              display: "none",
+            },
+          }}
+          centered
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+          style={{ height: "100%", display: "flex", alignItems: "center" }}
+        >
+          {tabsData.map((i, index) => {
+            let color = "grey";
+            if (currentTabID === i.name) {
+              color = "#000000";
+            }
+            return (
+              <Tab
+                key={index}
+                {...a11yProps(index)}
+                component={() => (
+                  <Button
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      padding: "10px",
+                      margin: "18px",
+                      textAlign: "center",
+                      justifyContent: "center",
+                    }}
+                    onClick={() => {
+                      setValue(index);
+                      setCurrentTabID(i.name);
+                    }}
+                  >
+                    <label style={{ color: color, cursor: "pointer" }}>
+                      {i.name}
+                    </label>
+                  </Button>
+                )}
+              />
+            );
+          })}
+        </Tabs>
       </div>
 
       {tabsData.map((element, index) => {
-      return (
-        <TabPanel key={index} value={value} index={index}>
-          <element.component />
-        </TabPanel>
-      );
+        return (
+          <TabPanel key={index} value={value} index={index}>
+            <element.component />
+          </TabPanel>
+        );
       })}
-
     </div>
   );
 };
--- frontend/src/components/TeacherHomePage/HomePage.js
+++ frontend/src/components/TeacherHomePage/HomePage.js
@@ -3,23 +3,21 @@ import { useState } from "react";
 import HomePageStyling from "@styles/HomePage/HomePage.module.scss";
 import { connect } from "react-redux";
 import { getTeacherFeed } from "redux/actions/teacher.action";
-import { TabPanel, a11yProps } from "../MaterialCustomComponents/TabPanel";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import Button from "@mui/material/Button";
-import Feed from "./Feed";
-import QuestionBank from "./QuestionBank/QuestionBank"
-import Quizes from "./Quizes"
-import Assignments from "./Assignments"
-import FeedClassroomCard from "@components/FeedClassroomCard/FeedClassroomCard";
-import FeedSectionCard from "@components/FeedSectionCard/FeedSectionCard";
+import {
+  TabPanel,
+  a11yProps,
+} from "@components/MaterialCustomComponents/TabPanel";
+import { Tabs, Tab, Button } from "@mui/material";
+import Feed from "@components/TeacherHomePage/Feed";
+import QuestionBank from "@components/TeacherHomePage/QuestionBank/QuestionBank";
+import Quizzes from "@components/TeacherHomePage/Quizzes";
+import Assignments from "@components/TeacherHomePage/Assignments";
 import Navbar from "@components/Navbar/Navbar";
-
 
 let tabsData = [
   { name: "Feed", component: Feed },
   { name: "Question Bank", component: QuestionBank },
-  { name: "Quizes", component: Quizes },
+  { name: "Quizzes", component: Quizzes },
   { name: "Assignments", component: Assignments },
 ];
 
@@ -39,55 +37,53 @@ const HomePage = ({ getTeacherFeed }) => {
     <div style={{ display: "flex", flexDirection: "column" }}>
       <Navbar />
       <div className={HomePageStyling.subTabMenu}>
-          <Tabs
-            TabIndicatorProps={{
-              style: {
-                display: "none",
-              },
-            }}
-            centered
-            value={value}
-            onChange={handleChange}
-            aria-label="basic tabs example"
-            style={{ height: "100%", display: 'flex', alignItems: 'center' }}
-          >
-            {tabsData.map((i, index) => {
-              let color = "grey";
-              if (currentTabID === i.name) {
-                color = "#000000";
-              }
-              return (
-                <Tab
-                  key={index}
-                  {...a11yProps(index)}
-                  component={() => (
-                    <Button
-                      style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        padding: "10px",
-                        margin: "15px",
-                        textAlign: "center",
-                        justifyContent: "center"
-                      }}
-                      onClick={() => {
-                        setValue(index);
-                        setCurrentTabID(i.name);
-                      }}
-                    >
-                      <label
-                        style={{ color: color, cursor: 'pointer' }}
-                      >
-                        {i.name}
-                      </label>
-                    </Button>
-                  )}
-                />
-              );
-            })}
-          </Tabs>
+        <Tabs
+          TabIndicatorProps={{
+            style: {
+              display: "none",
+            },
+          }}
+          centered
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+          style={{ height: "100%", display: "flex", alignItems: "center" }}
+        >
+          {tabsData.map((i, index) => {
+            let color = "grey";
+            if (currentTabID === i.name) {
+              color = "#000000";
+            }
+            return (
+              <Tab
+                key={index}
+                {...a11yProps(index)}
+                component={() => (
+                  <Button
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      padding: "10px",
+                      margin: "15px",
+                      textAlign: "center",
+                      justifyContent: "center",
+                    }}
+                    onClick={() => {
+                      setValue(index);
+                      setCurrentTabID(i.name);
+                    }}
+                  >
+                    <label style={{ color: color, cursor: "pointer" }}>
+                      {i.name}
+                    </label>
+                  </Button>
+                )}
+              />
+            );
+          })}
+        </Tabs>
       </div>
-          
+
       {tabsData.map((element, index) => {
         return (
           <TabPanel key={index} value={value} index={index}>
