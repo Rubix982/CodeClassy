51dc33cd036c711fb0e4c9a5abdf4a1d3096ad61
Tashik <k180142@nu.edu.pk>
2022-03-22 04:36:02 +0500

    removed sub header

--- frontend/src/components/QuizPages/QuizCreation.js
+++ frontend/src/components/QuizPages/QuizCreation.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Navbar from "../Navbar/Navbar"
 import Link from 'next/link'
 
 export default function QuizCreation() {
   return (
     <div>
-        Hello World!
+        <Navbar/>
     </div>
   );
 }
