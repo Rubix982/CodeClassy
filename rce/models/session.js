150e77831ec205c23eef04885a4bcecd5167d93c
Tashik <k180142@nu.edu.pk>
2022-05-08 13:28:56 +0500

    removed rce

--- rce/models/session.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose');
-
-var sessionSchema = new mongoose.Schema({
-  sourceCode: String
-});
-
-module.exports = mongoose.model('Session', sessionSchema);
--- rce/routes/about.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET about page. */
-router.get('/', function(req, res, next) {
-  res.render('about', { title: 'CodaSession', subtitle: 'Distributed yet highly collaborative coding' });
-});
-
-module.exports = router;
