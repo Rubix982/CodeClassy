e12d5e0b98e274082f33108c97ac54fc9820f032
Tashik <k180142@nu.edu.pk>
2022-05-09 01:39:10 +0500

    added editor

--- frontend/src/components/StudentHomePage/Assignments.js
+++ frontend/src/components/StudentHomePage/Assignments.js
@@ -1,10 +1,60 @@
 import React from "react";
+import Link from 'next/link'
+import Image from "next/image";
+import AssignmentImage from "../../../public/assets/images/assignment.png"
+import AssignmentStyles from "../../../styles/TeacherHomePage/assignments.module.css"
+import Button from '@mui/material/Button';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+
+
+let assignment_Data = [
+  {"Name": "First", "Subject": "Binary Search Tree" },
+  {"Name": "Second", "Subject": "Queues" },
+  {"Name": "Third", "Subject": "Stack" },
+  {"Name": "Fourth", "Subject": "Red Black Tree" },
+  {"Name": "Fifth", "Subject": "Hashing" },
+  {"Name": "Sixth", "Subject": "Sorting" },
+]
+
+
 
 export default function Assignments() {
-    return (
-      <>
-        Assignments
-      </>
-    );
+  return (
+    <div className={AssignmentStyles.container}>
+
+      <div className={AssignmentStyles.assignmentzes}>
+        {
+          assignment_Data.map((item, index) => {
+            return (
+              <div key={index} className={AssignmentStyles.assignmentItem}>
+                <div style={{margin:'7px'}}>
+                  <Image
+                    height={30}
+                    width={30}
+                    src={AssignmentImage}
+                    alt="Quiz-image"
+                  />
+                </div>
+                <div style={{marginLeft: '20px'}} className={AssignmentStyles.assignmentName}>
+                  {item.Subject} 
+                </div>
+
+                <div className={AssignmentStyles.assignmentView}>
+                  <Link href="/assignment/start">
+                    <a style={{textDecoration: 'none'}}>
+                      <Button style={{ height: '40px', backgroundColor: 'orange'}} variant="contained"> 
+                        <div style={{height: '100%', paddingRight: '5px'}}><KeyboardArrowRightIcon/></div> Start Coding
+                      </Button>
+                    </a>
+                  </Link>
+                </div>
+
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
+  )
   }
   
--- frontend/src/components/StudentHomePage/Quizzes.js
+++ frontend/src/components/StudentHomePage/Quizzes.js
@@ -1,5 +1,59 @@
 import React from "react";
+import Link from 'next/link'
+import Image from "next/image";
+import QuizImage from "../../../public/assets/images/quiz.png"
+import QuizStyles from "../../../styles/TeacherHomePage/quizzes.module.css"
+import Button from '@mui/material/Button';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+
+
+let Quiz_Data = [
+  {"Name": "First", "Subject": "DevOps" },
+  {"Name": "Second", "Subject": "Cloud Computing" },
+  {"Name": "Third", "Subject": "Cyber Security" },
+  {"Name": "Fourth", "Subject": "Information Processing Technique" },
+  {"Name": "Fifth", "Subject": "DevOps" },
+  {"Name": "Sixth", "Subject": "Linear Algebra" },
+]
 
 export default function Quizzes() {
-  return <>Quizzes</>;
+  return (
+    <div className={QuizStyles.container}>
+
+      <div className={QuizStyles.Quizzes}>
+        {
+          Quiz_Data.map((item, index) => {
+            return (
+              <div key={index} className={QuizStyles.quizItem}>
+                <div style={{margin:'7px'}}>
+                  <Image
+                    height={30}
+                    width={30}
+                    src={QuizImage}
+                    alt="Quiz-image"
+                  />
+                </div>
+
+                <div style={{marginLeft: '20px'}} className={QuizStyles.quizName}>
+                  {item.Subject} 
+                </div>
+
+                <div className={QuizStyles.quizView}>
+                  <Link href="/quiz/view">
+                    <a style={{textDecoration: 'none'}}>
+                      <Button style={{ height: '40px', backgroundColor: 'orange'}} variant="contained"> 
+                        <div style={{height: '100%', paddingRight: '5px'}}><KeyboardArrowRightIcon/></div> Start
+                      </Button>
+                    </a>
+                  </Link>
+                </div>
+
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
+  )
+
 }
