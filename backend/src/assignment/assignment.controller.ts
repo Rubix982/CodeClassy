893aa38a13304162c400fc1da1381b2238e6dd3e
Saif Ul Islam <saifulislam84210@gmail.com>
2022-06-01 03:29:56 +0500

    :sparkles: :construction: feat: added GET, POST, DELETE request for assignment
    Signed-off-by: Saif Ul Islam <saifulislam84210@gmail.com>

--- backend/src/assignment/assignment.controller.ts
+++ backend/src/assignment/assignment.controller.ts
@@ -1,7 +1,16 @@
+import { AssignmentRequestDTO } from './assignment.dto';
 import { JWTPayload } from 'src/auth/signin.dto';
 import { TeacherGuard } from 'src/teacher/teacher.guard';
 import { AssignmentService } from 'src/assignment/assignment.service';
-import { Body, Controller, Get, Param, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  Delete,
+  Param,
+} from '@nestjs/common';
 import { RequestDecodedMember } from 'src/decorators/member.decorator';
 import { AppGuard } from 'src/app/app.guard';
 
@@ -19,6 +28,32 @@ export class AssignmentController {
     return {
       msg: 'Successfully fetched assignments created by teacher',
       assignmentResults,
+    };
+  }
+
+  @Post()
+  async createAssignment(
+    @RequestDecodedMember() __member: JWTPayload,
+    @Body() __requestBody: AssignmentRequestDTO,
+  ) {
+    const createAssignmentResults =
+      await this.assignmentService.createAssignment(
+        __member.email,
+        __requestBody,
+      );
+
+    return {
+      msg: `Successfully created assignment`,
+      createAssignmentResults,
+    };
+  }
+
+  @Delete(':id')
+  async deleteAssignment(@Param('id') __assignmentId: string) {
+    await this.assignmentService.deleteAssignment(__assignmentId);
+
+    return {
+      msg: `Successfully deleted assignment with id ${__assignmentId}`,
     };
   }
 }
--- backend/src/assignment/assignment.service.ts
+++ backend/src/assignment/assignment.service.ts
@@ -1,4 +1,11 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { CodingQuestion } from 'src/entities/coding-question.entity';
+import { AssignmentRequestDTO } from './assignment.dto';
+import { Teacher } from 'src/entities/teacher.entity';
+import {
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Assignment } from 'src/entities/assignment.entity';
 import { Repository } from 'typeorm';
@@ -8,6 +15,10 @@ export class AssignmentService {
   constructor(
     @InjectRepository(Assignment)
     private readonly assignmentRepository: Repository<Assignment>,
+    @InjectRepository(Teacher)
+    private readonly teacherRepository: Repository<Teacher>,
+    @InjectRepository(CodingQuestion)
+    private readonly codingQuestionRepository: Repository<CodingQuestion>,
   ) {}
 
   async getAllAssignmentsByTeacher(__email: string) {
@@ -24,5 +35,43 @@ export class AssignmentService {
         `Could not find assignment for user with email: ${__email}`,
       ]);
     }
+  }
+
+  async createAssignment(__email: string, __requestBody: AssignmentRequestDTO) {
+    try {
+      const teacher = await this.teacherRepository.findOneOrFail({
+        where: { email: __email },
+      });
+
+      const codingQuestion = await this.codingQuestionRepository.findOneOrFail({
+        where: { id: __requestBody.codingQuestionId },
+      });
+
+      const assignment = this.assignmentRepository.create({
+        name: __requestBody.name,
+        codingQuestion: codingQuestion,
+        createdBy: teacher,
+      });
+
+      await this.assignmentRepository.save(assignment);
+    } catch (error) {}
+
+    throw new BadRequestException([
+      `Could not successfully create the assignment with name, ${__requestBody.name}`,
+    ]);
+  }
+
+  async deleteAssignment(__assignmentId: string) {
+    try {
+      await this.assignmentRepository
+        .createQueryBuilder()
+        .delete()
+        .where('id = :id', { id: __assignmentId })
+        .execute();
+    } catch (error) {}
+
+    throw new BadRequestException([
+      `Could not successfully delete the assignment with id, ${__assignmentId}`,
+    ]);
   }
 }
