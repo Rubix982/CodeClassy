d2a27337f4d815dc5f25f537006242f6dce8edb2
Muhammad Hassan Zahid <hassanzhd2000@gmail.com>
2022-06-05 08:51:32 +0500

    On branch question-bank-integration Changes to be committed:
    - Updated MCQ component for integration
    - Added local component level state using context where required

--- frontend/src/components/Question/Problem.js
+++ frontend/src/components/Question/Problem.js
@@ -1,14 +1,14 @@
-import * as React from 'react';
-import RichTextEditor from "../RichTextEditor/RichTextEditor"
-import TextField from '@mui/material/TextField';
+import * as React from "react";
+import RichTextEditor from "../RichTextEditor/RichTextEditor";
+import TextField from "@mui/material/TextField";
 
 const Problem = () => {
-    const [value, setValue] = React.useState('');
-    return ( 
-     <>
-        <h4 style={{margin: '10px', color: '#444444'}}> Problem </h4>
-        <TextField
-        style={{margin: '13px', width: '88%'}}
+  const [value, setValue] = React.useState("");
+  return (
+    <>
+      <h4 style={{ margin: "10px", color: "#444444" }}> Problem </h4>
+      <TextField
+        style={{ margin: "13px", width: "88%" }}
         fullWidth
         id="standard-multiline-flexible"
         label="Title"
@@ -17,10 +17,10 @@ const Problem = () => {
         value={value}
         onChange={(e) => setValue(e.target.value)}
         variant="standard"
-        />
-        <RichTextEditor/>
-     </>
-    );
-}
- 
+      />
+      <RichTextEditor contextKey="questionBody" arrayType={false} />
+    </>
+  );
+};
+
 export default Problem;
--- frontend/src/components/RichTextEditor/RichTextEditor.js
+++ frontend/src/components/RichTextEditor/RichTextEditor.js
@@ -1,33 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 const Editor = dynamic(
-  () => import('react-draft-wysiwyg').then(mod => mod.Editor),
+  () => import("react-draft-wysiwyg").then((mod) => mod.Editor),
   { ssr: false }
-)
-import { EditorState, convertToRaw } from "draft-js"
-import '../../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-import dynamic from 'next/dynamic';
+);
+import { EditorState, convertToRaw } from "draft-js";
+import "../../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import dynamic from "next/dynamic";
+import { QuestionContext } from "@components/Question/Question";
 
-const RichTextEditor = () => {
-
+const RichTextEditor = (props) => {
+  const context = useContext(QuestionContext);
   const [editorState, setEditorState] = useState(EditorState.createEmpty());
 
   const onEditorStateChange = (editorState) => {
     setEditorState(editorState);
-  }
+    const JSONContent = JSON.stringify(editorState.getCurrentContent());
+    if (props.arrayType) {
+      context[props.contextKey].setter(
+        context[props.contextKey].state.map((element, index) => {
+          if (index === props.contextIndex) {
+            return {
+              body: JSONContent,
+              isCorrect: element.isCorrect,
+            };
+          } else {
+            return element;
+          }
+        })
+      );
+    } else {
+      context[props.contextKey].setter(JSONContent);
+    }
+  };
 
-  // console.log(convertToRaw(editorState.getCurrentContent()).blocks[0].text)
+  // console.log(editorState.getCurrentContent()).blocks[0].text)
   // for text from richtext ^^^
 
-  return(
-    <div 
-      style={{ 
-      margin: '12px', 
-      width: '87%',
-      padding: '3px', 
-      border: '1px solid #d0d2d2', 
-      borderRadius: '5px',
-      backgroundColor: '#f7f7f7'
-      }}>
+  return (
+    <div
+      style={{
+        margin: "12px",
+        width: "87%",
+        padding: "3px",
+        border: "1px solid #d0d2d2",
+        borderRadius: "5px",
+        backgroundColor: "#f7f7f7",
+      }}
+    >
       <Editor
         editorState={editorState}
         toolbarClassName="toolbarClassName"
